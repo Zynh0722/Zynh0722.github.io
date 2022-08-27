11e809310a048d270b4d835b6f64d1ebf052cc5d
Zynh0722 <Zynh0722@gmail.com>
2022-08-26 20:34:28 -0700

    Adjust site description

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Zynh\'s Shenanigans';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'A place for me to put my thoughts down and recount my journey. And while I\'m here I might as well share it with you guys! - Zynh Ludwig';
