f41d0b1322334955f399b8f18fa8859ce6e605aa
Zynh0722 <Zynh0722@gmail.com>
2022-08-26 21:54:09 -0700

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   markdown: {
     drafts: true
   },
