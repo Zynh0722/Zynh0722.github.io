a6fa7e00795075329a3666eac0f0cea5f8db6a91
Zynh0722 <Zynh0722@gmail.com>
2022-08-27 14:57:27 -0700

    p5 Implemented!!!

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import robotsTxt from 'astro-robots-txt';
+import compress from 'astro-compress';
 
-import readTimePlugin from './src/lib/readTimePlugin.mjs';
+import readTimePlugin from './src/lib/readTime.mjs';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx({ remarkPlugins: { extends: [readTimePlugin] } }), sitemap()],
+  integrations: [mdx({ remarkPlugins: { extends: [readTimePlugin] } }), sitemap(), compress(), robotsTxt()],
   markdown: {
     drafts: true,
     remarkPlugins: [readTimePlugin],
